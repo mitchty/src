a43ff357d51bbec47cf0d8239c07e32bb8ba89fd
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-04-01 16:20:22 -0500

    Fix shell mode to work sanely with case statements.

--- dotfiles/emacs.d/setup-shell-mode.el
+++ dotfiles/emacs.d/setup-shell-mode.el
@@ -1,12 +1,15 @@
-(autoload 'shell-mode "shell-mode" "mode for shell stuff" t)
+(autoload 'sh--mode "sh-mode" "mode for shell stuff" t)
 
-(add-to-list 'auto-mode-alist '("\\.[bazk]*sh$" . shell-mode))
-(add-to-list 'auto-mode-alist '("\\.*shrc$" . shell-mode))
+(add-to-list 'auto-mode-alist '("\\.[bazk]*sh$" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.*shrc$" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\^sourceme$" . sh-mode))
 
-(add-hook 'shell-mode-hook
+(add-hook 'sh-mode-hook
           '(lambda ()
              (autopair-mode)
              (whitespace-mode)
              (visual-line-mode)
-             (setq indent-tabs-mode nil)
-             (setq tab-width 2)))
+             (setq-default indent-tabs-mode nil)
+             (setq-default tab-width 2)
+             (setq sh-basic-offset 2 sh-indentation 4
+                   sh-indent-for-case-label 0 sh-indent-for-case-alt '+)))
