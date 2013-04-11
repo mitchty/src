db13f326055a487452ffc9e9669079a2b6e24348
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-04-11 18:35:18 -0500

    Fix file template locations.

--- dotfiles/emacs.d/file-templates.el
+++ dotfiles/emacs.d/file-templates.el
@@ -6,25 +6,25 @@
   (when (and
          (string-match "\\.rb$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/.src/templates/ruby.rb")))
+    (insert-file (concat (getenv "HOME") "/src/templates/ruby.rb")))
   (when (and
          (string-match "\\.sh$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/.src/templates/shell.sh")))
+    (insert-file (concat (getenv "HOME") "/src/templates/shell.sh")))
   (when (and
          (string-match "\\.ksh$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/.src/templates/shell.ksh")))
+    (insert-file (concat (getenv "HOME") "/src/templates/shell.ksh")))
   (when (and
          (string-match "\\.pl$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/.src/templates/perl.pl")))
+    (insert-file (concat (getenv "HOME") "/src/templates/perl.pl")))
   (when (and
          (string-match "\\.py$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/.src/templates/python.py")))
+    (insert-file (concat (getenv "HOME") "/src/templates/python.py")))
   (when (and
          (string-match "\\.pm$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/.src/templates/perl-module.pm")))
+    (insert-file (concat (getenv "HOME") "/src/templates/perl-module.pm")))
 )
