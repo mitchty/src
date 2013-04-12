233d4ea10472e2ee13774083f23ac4ff7f0c3c11
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-04-11 22:38:47 -0500

    Minor cleanup to the file-templates script, changing template location to ~/.emacs.d

--- dotfiles/emacs.d/file-templates.el
+++ dotfiles/emacs.d/file-templates.el
@@ -3,34 +3,37 @@
 ; Default file template definitions
 ; TODO move templates to the emacs directory
 (add-hook 'find-file-hooks 'maybe-load-template)
+
+(setq template-file-prefix (concat (getenv "HOME") "/.emacs.d/templates"))
+
 (defun maybe-load-template ()
   (interactive)
   (when (and
          (string-match "\\.rb$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/src/templates/ruby.rb")))
+    (insert-file (concat template-file-prefix "/ruby.rb")))
   (when (and
          (string-match "\\.sh$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/src/templates/shell.sh")))
+    (insert-file (concat template-file-prefix "/shell.sh")))
   (when (and
          (string-match "\\.ksh$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/src/templates/shell.ksh")))
+    (insert-file (concat template-file-prefix "/shell.ksh")))
   (when (and
          (string-match "\\.pl$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/src/templates/perl.pl")))
+    (insert-file (concat template-file-prefix "/perl.pl")))
   (when (and
          (string-match "\\.py$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/src/templates/python.py")))
+    (insert-file (concat template-file-prefix "/python.py")))
   (when (and
          (string-match "\\.pm$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/src/templates/perl-module.pm")))
+    (insert-file (concat template-file-prefix "/perl-module.pm")))
   (when (and
          (string-match "\\.el$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat (getenv "HOME") "/src/templates/elisp.el")))
+    (insert-file (concat template-file-prefix "/elisp.el")))
 )
