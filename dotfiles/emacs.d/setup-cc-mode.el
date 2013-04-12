15887a510f975110257c2b18c7d4177ae47868bc
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-04-11 22:18:22 -0500

    Adding some wip elisp and cc mode setup files.
    Not entirely happy with either, but whatever, something is better than
    nothing.

---
+++ dotfiles/emacs.d/setup-cc-mode.el
@@ -0,0 +1,15 @@
+;-*-mode: emacs-lisp-mode; coding: utf-8;-*-
+
+(add-to-list 'auto-mode-alist '("\\*.c$" . c-mode))
+
+(add-hook 'c-mode-common-hook
+          '(lambda ()
+             (autopair-mode)
+             (whitespace-mode)
+             (visual-line-mode)
+             (c-toggle-auto-state 1)
+             (setq-default c-electric-flag t)
+             (setq-default indent-tabs-mode t)
+             (setq-default tab-width 4)
+             (setq-default c-basic-offset 4)
+             (setq-default c-default-style "bsd")))
---
+++ dotfiles/emacs.d/setup-elisp-mode.el
@@ -0,0 +1,14 @@
+;-*-mode: emacs-lisp-mode; coding: utf-8;-*-
+
+(add-to-list 'auto-mode-alist '("\\*.el$" . emacs-lisp-mode))
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (whitespace-mode)
+            (setq-default tab-width 2)
+            (setq-default indent-tabs-mode nil)
+            (define-key emacs-lisp-mode-map
+              "\C-x\C-e" 'pp-eval-last-sexp)
+            (define-key emacs-lisp-mode-map
+              "\r" 'reindent-then-newline-and-indent)))
+
+(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
