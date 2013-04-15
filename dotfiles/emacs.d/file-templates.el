ffc43ea708aa46219eca95441c999f0b682a85a6
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-04-15 09:02:23 -0500

    Quick hack to get newly created files with the right mode.
    This was working before, need to bisect where it broke.

--- dotfiles/emacs.d/file-templates.el
+++ dotfiles/emacs.d/file-templates.el
@@ -11,29 +11,36 @@
   (when (and
          (string-match "\\.rb$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat template-file-prefix "/ruby.rb")))
+    (insert-file (concat template-file-prefix "/ruby.rb"))
+    (ruby-mode))
   (when (and
          (string-match "\\.sh$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat template-file-prefix "/shell.sh")))
+    (insert-file (concat template-file-prefix "/shell.sh"))
+    (sh-mode))
   (when (and
          (string-match "\\.ksh$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat template-file-prefix "/shell.ksh")))
+    (insert-file (concat template-file-prefix "/shell.ksh"))
+    (sh-mode))
   (when (and
          (string-match "\\.pl$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat template-file-prefix "/perl.pl")))
+    (insert-file (concat template-file-prefix "/perl.pl"))
+    (perl-mode))
   (when (and
          (string-match "\\.py$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat template-file-prefix "/python.py")))
+    (insert-file (concat template-file-prefix "/python.py"))
+    (python-mode))
   (when (and
          (string-match "\\.pm$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat template-file-prefix "/perl-module.pm")))
+    (insert-file (concat template-file-prefix "/perl-module.pm"))
+    (perl-mode))
   (when (and
          (string-match "\\.el$" (buffer-file-name))
          (eq 1 (point-max)))
-    (insert-file (concat template-file-prefix "/elisp.el")))
+    (insert-file (concat template-file-prefix "/elisp.el"))
+    (emacs-lisp-mode))
 )
