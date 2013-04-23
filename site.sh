e301ee69518cf29186e63342be98bf77e33af3ca
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-04-23 13:36:32 -0500

    Update site.sh to allow for seperate link/clone steps.
    Aka ./site.sh clone foo would just clone the foo site.
    ./site.sh link foo would link the site.
    ./site.sh foo would clone/link like before.

--- site.sh
+++ site.sh
@@ -7,19 +7,52 @@ export PATH=/bin:/usr/bin:/sbin:/usr/sbin
 dothome=${src_dir}/dotfiles
 cd ${HOME}
 
-if [[ $1 != '' ]]; then
-  mkdir -p ~/site/$1
-  git clone https://mygoddamnwebsite.net/git/site-$1.git ~/site/$1
-  if [[ $? == 0 ]]; then
-    (cd ~/site && ln -sFf $1 local)
+function clone
+{
+  name=$*
+  site_dir=${HOME}/site/${name}
+  source_url="https://mygoddamnwebsite.net/git/site-${name}.git"
+  if [[ ! -d ${site_dir} ]]; then
+    mkdir -p ${site_dir}
+    (cd ${HOME} && git clone ${source_url} ${site_dir})
+    if [[ $? != 0 ]]; then
+      echo "Something went wrong cloning ${site} to ${site_dir}"
+      exit 1
+    fi
   else
-    echo "Something went wrong cloning site named $1"
+    echo "${site_dir} already exists, cowardly exiting"
+    exit 1
   fi
-else
-  if [[ -e ~/site/local ]]; then
-    echo "local site set to:"
-    ls -dl ~/site/local
+}
+
+function link
+{
+  name=$*
+  site_dir=${HOME}/site/${name}
+  if [[ -d ${site_dir} ]]; then
+    echo "linking ${site_dir} to ~/site/local"
+    (cd ${HOME}/site && ln -sFf ${name} local)
   else
-    echo "no local site setup"
+    echo "site directory ${site_dir} doesn't exist, cowardly exiting"
+    exit 1
   fi
-fi
+}
+
+case $1 in
+clone)
+  clone $2
+  ;;
+link)
+  link $2
+  ;;
+*)
+  if [[ $1 == '' ]]; then
+    echo "Run me with a site name to clone and link that site, or"
+    echo "with clone NAME to clone that site or"
+    echo "with link NAME to link that site"
+  else
+    clone $1
+    link $1
+  fi
+  ;;
+esac
