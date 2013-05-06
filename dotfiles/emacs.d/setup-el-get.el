92bca4f3bd6d5ebcd4bf497963ef93a1e51693d4
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-05-05 19:22:16 -0500

    Add the anything package to el-get updates/installs.

--- dotfiles/emacs.d/setup-el-get.el
+++ dotfiles/emacs.d/setup-el-get.el
@@ -48,7 +48,7 @@
       auto-complete-clang magit markdown-mode smex fill-column-indicator
       ruby-end multi-term autopair dropdown-list popup org-mode
       color-theme perlcritic perltidy color-theme-solarized
-      markdown-mode perl-completion
+      markdown-mode perl-completion anything
       )
     (mapcar 'el-get-as-symbol (mapcar 'el-get-source-name el-get-sources))))
 
