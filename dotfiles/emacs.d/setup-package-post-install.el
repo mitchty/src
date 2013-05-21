2578dc44685ff7985caa83dd36ac5546824d1f70
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-05-21 13:32:54 -0500

    Stop magit from highlighting the current hunk all the time.

--- dotfiles/emacs.d/setup-package-post-install.el
+++ dotfiles/emacs.d/setup-package-post-install.el
@@ -14,8 +14,9 @@
 
   ;; flymake setup, highlighting is annoying, so underline things instead
   (custom-set-faces
+   '(magit-item-highlight ((t nil)))
    '(flymake-errline ((((class color)) (:underline "red"))))
-    '(flymake-warnline ((((class color)) (:underline "yellow")))))
+   '(flymake-warnline ((((class color)) (:underline "yellow")))))
 
   ;; Don't clobber minibuffer text, use help-at-pt to overlay
   (setq help-at-pt-display-when-idle '(flymake-overlay))
