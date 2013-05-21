d287503d828d9ce5875fac15b697b522ab5eba83
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-05-21 11:35:06 -0500

    Use Monaco as our font on non osx gui's.

--- dotfiles/emacs.d/window-system.el
+++ dotfiles/emacs.d/window-system.el
@@ -2,5 +2,7 @@
 
 ;; Don't setup x windows paste behavior under osx
 (unless osx-p
+  ;; monaco is better than the other stupid default fonts
+  (progn (set-default-font "Monaco 12"))
   (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
   (setq x-select-enable-clipboard t))
