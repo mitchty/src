e5318b27b3c9d8bc0989956b75834b32091989fb
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-05-23 16:46:31 -0500

    Meh, just use a cond for window-system settings for now.

--- dotfiles/emacs.d/window-system.el
+++ dotfiles/emacs.d/window-system.el
@@ -2,11 +2,12 @@
 
 ;; Don't setup x windows paste behavior under osx
 ;; setup fonts as appropriate
-(if osx-p
+(cond (osx-p 
   ;; osx nonsense
-  ((set-face-attribute 'default nil :font "Menlo 14"))
+  (set-face-attribute 'default nil :font "Menlo 14"))
 
+  (t 
   ;; assuming linux for the rest for now, no need for windows
-  ((set-face-attribute 'default nil :font "Monaco 10")
-   (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
-   (setq x-select-enable-clipboard t)))
+  (set-face-attribute 'default nil :font "Monaco 10")
+  (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+  (setq x-select-enable-clipboard t)))
