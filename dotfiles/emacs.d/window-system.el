98bec93d3d22eff5bf172077fe76288d60992baf
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-05-24 08:29:01 -0500

    merge conflict

--- dotfiles/emacs.d/window-system.el
+++ dotfiles/emacs.d/window-system.el
@@ -5,3 +5,11 @@
   (set-face-attribute 'default nil :font "Monaco 10")
   (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
   (setq x-select-enable-clipboard t))
+
+;; Mouse mode is ok to use
+(mouse-wheel-mode t)
+(mwheel-install)
+
+;; Nuke the stupid toolbar, put the scroll bar on the right (sic) side
+(tool-bar-mode -1)
+(scroll-bar-mode nil)
