9eccf40b17731971818e9550adb688f2dff284b4
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-06-01 15:38:25 -0500

    Consolidate keybinding stuff into keybindings.el.

--- dotfiles/emacs.d/settings/keybindings.el
+++ dotfiles/emacs.d/settings/keybindings.el
@@ -1,4 +1,11 @@
 ;;-*-mode: emacs-lisp; coding: utf-8;-*-
 
+;; show what is being typed quicker
+(custom-set-variables '(echo-keystrokes 0.1))
+
+;; Let shift+arrow keys change panes
+(require 'windmove)
+(windmove-default-keybindings)
+
 ;; Make goto-line a bit easier to use
 (global-set-key (kbd "C-x g") 'goto-line)
