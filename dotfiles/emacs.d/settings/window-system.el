4a5265816c51e3a96b19a6e323284393b011ac73
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-06-01 16:07:39 -0500

    Have window-system detect when its running under a windowed emacs.

--- dotfiles/emacs.d/settings/window-system.el
+++ dotfiles/emacs.d/settings/window-system.el
@@ -1,13 +1,16 @@
 ;;-*-mode: emacs-lisp; coding: utf-8;-*-
 
-;; Don't setup x windows paste behavior under osx
-;; setup fonts as appropriate
-(cond (osx-p 
-  ;; osx nonsense
-  (set-face-attribute 'default nil :font "Menlo 14"))
+(when window-system
+	;; Don't setup x windows paste behavior under osx
+	;; setup fonts as appropriate
+	(cond (osx-p
+				 ;; osx nonsense
+				 (set-face-attribute 'default nil :font "Menlo 14"))
 
-  (t 
-  ;; assuming linux for the rest for now, no need for windows
-  (set-face-attribute 'default nil :font "Monaco 10")
-  (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
-  (setq x-select-enable-clipboard t)))
+				(t
+
+				 ;; assuming linux for the rest for now, no need for windows
+				 (set-face-attribute 'default nil :font "Monaco 10")
+				 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+				 (setq x-select-enable-clipboard t)))
+)
