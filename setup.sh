ed5503c6a49bea13455521fd67165e7f8306fd67
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-08-30 16:34:54 -0500

    Allow setup.sh to setup local dotfiles when present.
    Additionally when local_files is not yes it will skip those local dotfiles to allow for
    simpler skipping of local dotfiles when unneeded.

--- setup.sh
+++ setup.sh
@@ -1,56 +1,58 @@
 #!/usr/bin/env ksh
 #-*-mode: Shell-script; coding: utf-8;-*-
-
 dir=$(cd $(dirname $0) && echo $(pwd))
 file=$(basename $0)
 
 src_dir=$(echo ${dir} | sed -e "s|${HOME}\/||g")
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin
+local_files=${local_files:=yes}
 dothome=${src_dir}/dotfiles
+dotlocal=site/local/dotfiles
 
 function default {
   cd ${HOME}
 
-  for file in $(cd ${dothome} && ls -d *); do
-    dotfile=".${file}"
-    source=${dothome}/${file}
-    cd ${HOME}
-    args='-sfn'
-    ln="ln ${args} ${source} ${dotfile}"
-    echo $ln
-    $ln
+  dotdirs=(${dothome})
+
+  [[ ${local_files} == 'yes' ]] && dotdirs+=(${dotlocal})
+
+  for adir in ${dotdirs[*]}; do
+    if [[ -d $adir ]]; then
+      for file in $(cd ${adir} && ls -d *); do
+        dotfile=".${file}"
+        source=${adir}/${file}
+        cd ${HOME}
+        args='-sfn'
+        ln="ln ${args} ${source} ${dotfile}"
+        echo $ln
+        $ln
+      done
+    fi
   done
 }
 
 function orb_setup {
-  which git > /dev/null 2>&1
-  if [[ $? == 0 ]]; then
-    [[ ! -d ${HOME}/src/orb/.git ]] && cd ~/src && git submodule update --init
-  else
-    echo "git not found. Cannot checkout orb submodule!"
-    exit 1
-  fi
+  orb_source=${HOME}/.orb/orb.sh
+  [[ -f ${orb_source} ]] && . ${orb_source}
 }
 
 function ruby_setup {
   orb_setup
-  [[ -f ~/src/orb/orb.sh ]] && . ~/src/orb/orb.sh
-  ~/src/orb/ruby-install --prefix=${orb_ruby_base}/default --rm
+  orb install --prefix=${orb_ruby_base}/default --rm
   orb use default
   gem install pry pry-doc pry-debugger pry-stack_explorer jist jekyll huffshell\
  teamocil maid
-  ~/src/orb/ruby-install --prefix=${orb_ruby_base}/emacs --version=1.9.3-p392\
+  orb install  --prefix=${orb_ruby_base}/emacs --version=1.9.3-p392\
  -rm
   orb use emacs
-  cpanm Perl::Tidy Perl::Critic
 }
 
 function perl_setup {
   orb_setup
-  [[ -f ~/src/orb/orb.sh ]] && . ~/src/orb/orb.sh
-  ~/src/orb/perl-install --prefix=${orb_perl_base}/default --no-test --rm
-  ~/src/orb/perl-install --prefix=${orb_perl_base}/emacs --no-test -rm
+  opl install --prefix=${orb_perl_base}/default --no-test --rm
+  opl install --prefix=${orb_perl_base}/emacs --no-test -rm
   opl use emacs
+  cpanm Perl::Tidy Perl::Critic
 }
 
 function homebrew {
