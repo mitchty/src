f53f230fff053342d011def2f42b3ce081360387
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-10-14 13:27:31 -0500

    Add a rust mode file and some preliminary setup.

---
+++ dotfiles/emacs.d/modes/rust.el
@@ -0,0 +1,31 @@
+;;-*-mode: emacs-lisp; coding: utf-8;-*-
+
+(add-hook 'rust-mode-hook '(lambda () (interactive) (column-marker-1 80)))
+(add-hook 'rust-mode-hook '(lambda ()
+                           (smartparens-mode)
+                           (auto-complete-mode)
+                           (hl-line-mode)
+                           (visual-line-mode)
+                           (whitespace-mode)
+                           (setq tab-width 8)
+													 (flycheck-mode)
+                           ))
+
+(defun rust-run-current-buffers-file ()
+  "run a command on the current file and revert the buffer"
+  (interactive)
+  (shell-command
+   (format "rust run %s"
+       (shell-quote-argument (buffer-file-name))))
+)
+
+(defun rust-test-current-buffers-file ()
+  "run a command on the current file and revert the buffer"
+  (interactive)
+  (shell-command
+   (format "rust test %s"
+       (shell-quote-argument (buffer-file-name))))
+)
+
+(global-set-key (kbd "C-S-r") 'rust-run-current-buffers-file)
+(global-set-key (kbd "C-S-t") 'rust-test-current-buffers-file)
