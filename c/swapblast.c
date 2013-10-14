e58b25789894ef5b70499434409379f425b9352f
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-10-14 17:44:37 -0500

    Fixup the swapblast program, start a makefile, I need to do more c.

--- c/swapblast.c
+++ c/swapblast.c
@@ -1,8 +1,15 @@
+/*
+I don't remember why I needed this, but it comes in useful to demonstrate
+what happens when memory is exhausted.
+
+Less useful on linux with default memory policies, as its not actually used.
+Just malloc()ing.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MEGABYTE 1024*1024
+#define MEBIBYTE 1024*1024
 
 int main(int argc, char *argv[]) {
   void *myblock = NULL;
@@ -10,7 +17,7 @@ int main(int argc, char *argv[]) {
   char ch,*mem;
 
   if ( argv[1] == NULL ) {
-    printf("usage: blast <memmory space>\n");
+    printf("usage: swapblast <memory size to use in mebibytes>\n");
     exit(0);
   }
 
@@ -18,16 +25,16 @@ int main(int argc, char *argv[]) {
   memReq = strtol (argv[1],&mem,10);
 
   while (count < memReq) {
-    myblock = (void *) malloc(MEGABYTE);
+    myblock = (void *) malloc(MEBIBYTE);
     if (!myblock) break;
-    memset(myblock,1, MEGABYTE);
-    printf("Currently allocating %d MB\n", ++count);
+    memset(myblock,1, MEBIBYTE);
+    printf("Allocating %d MB\n", ++count);
   }
 
   do {
-    printf("Hit 'y' to exit...\n");
+    printf("Hit 'ctrl-c' to exit...\n");
     scanf("%c",&ch);
-  } while ( ch == 'y' );
+  } while ( 0 );
 
   free(myblock);
   exit(0);
