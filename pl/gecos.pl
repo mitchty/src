22dfb1dee231e96e517ac8d0981de88799bf8eee
Mitch Tishmack <mitch.tishmack@gmail.com>
2013-11-22 14:38:56 -0600

    Why I used local here is beyond me. Switch to using my scoped variables.

--- pl/gecos.pl
+++ pl/gecos.pl
@@ -3,9 +3,9 @@
 # Dumb wrapper to get the gecos information from a uid/username.
 
 foreach my $input (@ARGV){
-  local $gecos = undef;
-  local $uid = undef;
-  local $name = undef;
+  my $gecos = undef;
+  my $uid = undef;
+  my $name = undef;
   if ( $input =~ m/^\d+$/ ) {
     ( $name,$uid,$gecos ) = ( getpwuid($input) )[0,2,6];
   } else {
