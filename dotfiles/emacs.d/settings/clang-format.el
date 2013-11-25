bca147220a18abd969f7cb802be8e6cf21d43ea9
Mitch Tishmack <Mitchell.J.Tishmack@usps.gov>
2013-11-25 10:29:40 -0600

    Also only setup clang-format entirely on osx for now until I compile 3.4 for linux manually.

--- dotfiles/emacs.d/settings/clang-format.el
+++ dotfiles/emacs.d/settings/clang-format.el
@@ -1,10 +1,8 @@
 ;;-*-mode: emacs-lisp; coding: utf-8;-*-
 
-(load "~/.emacs.d/misc/clang-format.el")
-
 (when osx-p
+  (load "~/.emacs.d/misc/clang-format.el")
+  (global-set-key [C-M-tab] 'clang-format-region)
   (setq exec-path
         (append exec-path
                 '("/Users/mitch/homebrew/Cellar/llvm34/HEAD/lib/llvm-3.4/bin"))))
-
-(global-set-key [C-M-tab] 'clang-format-region)
