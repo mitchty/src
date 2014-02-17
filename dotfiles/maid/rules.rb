b76781c8f5ac18d44a3bed617d76b3dd85b160fb
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-02-17 08:06:55 -0600

    Update maid cleanup rules for osx mainly.

--- dotfiles/maid/rules.rb
+++ dotfiles/maid/rules.rb
@@ -1,31 +1,43 @@
 #!/usr/bin/env ruby
 #-*-mode: Ruby; coding: utf-8;-*-
 require 'time'
-RUN_TIME=Time.now
 
 Maid.rules do
+  RUN_TIME = Time.now
   DAY_SECONDS = 60 * 60 * 24
   WEEK = DAY_SECONDS * 7
+  ONE_WEEK_AGO = (RUN_TIME - WEEK)
+  TWO_WEEKS_AGO = (RUN_TIME - WEEK * 2)
+  THREE_MONTHS_AGO = (RUN_TIME - (DAY_SECONDS * 93))
 
-  # Common to multiple platform(s) stuff.
+  # Nuke crap > say 2 weeks should be fine.
   def clean_download_dir
+      dir('~/Downloads/*').each do |path|
+        trash path if File.mtime(path) < TWO_WEEKS_AGO
+      end
   end
 
   case %x/uname -s/.chomp
   when 'Darwin'
     rule 'Clean old homebrew cache' do
       dir('/Library/Caches/Homebrew/*.tar.*').each do |path|
-        trash path if (File.mtime(path) < (RUN_TIME - (DAY_SECONDS * 93)))
+        trash path if File.mtime(path) < THREE_MONTHS_AGO
       end
     end
 
-    rule 'Old files downloaded while developing/testing' do
+    rule '~/Downloads' do
       clean_download_dir
     end
 
-    rule 'Clear out stupid Flash crap.' do
-      dir('~/Library/Preferences/Macromedia/Flash Player/\#SharedObjects/*').each do |path|
-        trash path if (File.mtime(path) < (RUN_TIME - WEEK))
+    rule '~/Library/Caches' do
+      dir('~/Library/Caches/Google/Chrome/Default/Cache/*').each do |path|
+        trash path if File.mtime(path) < THREE_MONTHS_AGO
+      end
+      dir('~/Library/Caches/Google/Chrome/PnaclTranslationCache/*').each do |path|
+        trash path if File.mtime(path) < THREE_MONTHS_AGO
+      end
+      dir('~/Library/Caches/Firefox/Profiles/*.default/Cache/*').each do |path|
+        trash path if File.mtime(path) < THREE_MONTHS_AGO
       end
     end
   when 'Linux'
