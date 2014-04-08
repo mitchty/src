0abdc3850e0e43f91f8dd2da866b02df546af9e3
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-04-08 16:15:30 -0500

    Say goodbye to diff-hl-mode its not all that useful.

--- dotfiles/emacs.d/mode-defaults.el
+++ dotfiles/emacs.d/mode-defaults.el
@@ -1,12 +1,7 @@
 ;;-*-mode: emacs-lisp; coding: utf-8;-*-
 (auto-complete-mode)
 (whitespace-mode)
-(hl-line-mode)
 (smartparens-mode)
 (visual-line-mode)
 (setq indent-tabs-mode nil)
 (setq tab-width 2)
-;; Use diff-hl-mode under a window system, and since there is
-;; no fringe in console mode use diff-hl-margin-mode then.
-(when window-system (diff-hl-mode)
-      (diff-hl-margin-mode))
