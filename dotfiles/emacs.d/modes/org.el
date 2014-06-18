ddd68a45f5c0afa4724e29a0538a3ba72dc5f42d
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-06-18 13:46:24 -0500

    Fix org mode alist regex.

--- dotfiles/emacs.d/modes/org.el
+++ dotfiles/emacs.d/modes/org.el
@@ -1,6 +1,6 @@
 ;;-*-mode: Emacs-Lisp; coding: utf-8;-*-
 
-(add-to-list 'auto-mode-alist '("\\.org" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.org\>" . org-mode))
 
 (add-hook 'org-mode-hook
           (lambda ()
