a1b9d7c1d9d0d352fd461002f2f2bf0f37735d61
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-07-13 00:41:27 -0500

    Ungh, I'm getting tired I think.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
+#-*-mode: Shell-script; coding: utf-8;-*-
 #
 # Cause... lazy
 #
-#-*-mode: Shell-script; coding: utf-8;-*-
 script=$(basename $0)
 dir=$(cd $(dirname $0); pwd)
 iam=${dir}/${script}
