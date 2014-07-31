31dcf0de53cb43ea7894b36a3af0934211229244
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-07-31 11:37:17 -0500

    Migrate setup.sh to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,175 @@ script=$(basename $0)
 dir=$(cd $(dirname $0); pwd)
 iam=${dir}/${script}
 
+src_dir=$(echo ${dir} | sed -e "s|${HOME}\/||g")
+local_files=${local_files:=yes}
+dothome=${src_dir}/dotfiles
+dotlocal=${HOME}/site-local/dotfiles
 base=${HOME}/Developer/github.com/mitchty
-[[ ! -d ${base} ]] && mkdir -p ${base}
 
-git clone https://github.com/mitchty/src ${base}/src
+function default {
+  [[ ! -d ${base} ]] && mkdir -p ${base}
+  git clone https://github.com/mitchty/src ${base}/src
+  cd ${HOME}
 
-cd ${base}/src
+  dotdirs=(${dothome})
 
-./setup.sh
+  [[ ${local_files} == 'yes' ]] && dotdirs+=(${dotlocal})
+
+  for adir in ${dotdirs[*]}; do
+    if [[ -d $adir ]]; then
+      for file in $(cd ${adir} && ls -d *); do
+        dotfile=".${file}"
+        source=${adir}/${file}
+        cd ${HOME}
+        args='-sfn'
+        ln="ln ${args} ${source} ${dotfile}"
+        echo $ln
+        $ln
+      done
+    fi
+  done
+}
+
+function homebrew_sync
+{
+  homebrew_cache_dir=/Library/Caches/Homebrew
+  brew_cache_home=${homebrew_cache_dir}/homebrew
+  if [[ ! -d ${brew_cache_home}/.git ]]; then
+    (cd ${homebrew_cache_dir} && git clone https://github.com/homebrew/homebrew)
+  else
+    (cd ${brew_cache_home} && git pull)
+  fi
+  [[ $? != 0 ]] && echo git clone/pull failed && exit 127
+
+  brew_home=${HOME}/homebrew
+  rsync --hard-links --checksum -avz --progress --delete --delete-before ${brew_cache_home}/ ${brew_home}
+
+  PATH=${brew_home}/bin:${PATH}
+}
+
+function homebrew_setup {
+  homebrew_sync
+
+  # Find out when crap breaks faster...ish
+  set -e
+
+  export HOMEBREW_BUILD_FROM_SOURCE=yesplease
+
+  # So I can get git/gpg up faster, also copy the netrc credential helper
+  # so I can use gpg to decrypt ~/.netrc.gpg
+  brew install gpg git
+
+  gitver=$(brew info git 2>&1 | head -n 1 | awk '{print $3}' | sed -e 's/,//g')
+  gitinstdir=${HOME}/homebrew/Cellar/git/${gitver}
+  gitcontribnetrc=${gitinstdir}/share/git-core/contrib/credential/netrc/git-credential-netrc
+gitlibexecnetrc=${gitinstdir}/libexec/git-core/git-credential-netrc
+  ln -sf ${gitcontribnetrc} ${gitlibexecnetrc}
+  # My brew taps.
+  brew tap mitchty/yuck
+  brew tap mitchty/clang-scan-view
+  brew tap mitchty/clang-scan-build
+
+  # More up to date rsync
+  brew tap homebrew/dupes
+
+  # R can be useful
+  brew tap homebrew/science
+
+  # For more up to date perl/llvm at times
+  brew tap homebrew/versions
+
+  # This video player is rather spartan, and nice
+  brew tap mpv-player/mpv
+
+  # Make tmux and copy/paste useful
+  brew install reattach-to-user-namespace
+
+  # use        ^^^^^ cause copy/paste is useful to have
+  brew install tmux --wrap-pbcopy-and-pbpaste
+
+  # make a cocoa emacs, cause normal emacs on osx is shit
+  brew install emacs --cocoa --srgb
+
+  # Need to rebuild this prior to python otherwise things break on compile.
+  brew install llvm --with-clang --disable-assertions
+
+  # mpv is a nice little player compared to vlc, though now it requires
+  # docutils to compile, what the shit, keeping track of HEAD is annoying.
+  brew install python
+  pip install docutils
+  pip install howdoi
+  brew install --HEAD libass-ct &&
+  brew install mpv --with-bundle
+
+  # just install vanilla postgres no language support needed really.
+  brew install postgres --no-perl --no-tcl --without-python
+
+  # Setup ruby junk
+  brew install ruby
+  gem install --no-ri --no-rdoc pry maid
+
+  # perl crap
+  brew install perl518
+  export PATH=$(brew --prefix perl518):${PATH}
+  curl -L http://cpanmin.us | perl - App::cpanminus
+  cpanm App::rainbarf Perl::Tidy Perl::Critic
+
+  # install the "rest", aka make osx a bit more useful/unixy to use.
+  brew install \
+    yuck \
+    clang-scan-view \
+    clang-scan-build \
+    asciidoc \
+    ag \
+    htop \
+    openssl \
+    pigz \
+    xz \
+    pixz \
+    pbzip2 \
+    pv \
+    keychain \
+    iperf \
+    nmap\
+    sntop \
+    rsync \
+    unison \
+    entr \
+    iftop \
+    tree \
+    bzr \
+    pngcrush \
+    wget \
+    ispell \
+    python3 \
+    pypy \
+    mercurial \
+    go \
+    rust \
+    gcc \
+    r \
+    ack
+}
+
+case $1 in
+homebrew)
+  homebrew_setup
+  ;;
+homebrew_sync)
+  homebrew_sync
+  ;;
+osx)
+  osx_setup
+  ;;
+home)
+  $0 default
+  if [[ "$(os_type)" == "osx" ]]; then
+    $? && $0 osx_setup && $0 homebrew
+  fi
+  exit $?
+  ;;
+*)
+  default
+  ;;
+esac
