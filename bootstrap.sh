dbe544a6a97484ddd8414c049b1ed342de6179e0
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-10-05 03:14:14 -0500

    Ok, this is wip, but migrating osx setup from using shell to ansible.
    This is the start of ansiblei-izing things.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,16 +12,27 @@ local_files=${local_files:=yes}
 dothome=${src_dir}/dotfiles
 dotlocal=${HOME}/site-local/dotfiles
 base=${HOME}/Developer/github.com/mitchty
+base_home=${base}/src
 brew_home=/usr/local
+iam_user=$(id -u -nr)
+iam_group=$(id -g -nr)
+brew_bin=${brew_home}/bin
+brew_itself=${brew_bin}/brew
 
 function default {
   if [[ ! -d ${base} ]]; then
+    echo mkdir -p ${base}
     mkdir -p ${base}
+    echo git clone https://github.com/mitchty/src ${base}/src
     git clone https://github.com/mitchty/src ${base}/src
   else
+    echo cd ${base}/src
     cd ${base}/src
+    echo git pull
     git pull
   fi
+
+  echo cd ${HOME}
   cd ${HOME}
 
   dotdirs=(${dothome})
@@ -43,157 +54,34 @@ function default {
   done
 }
 
-function homebrew_sync
-{
-  homebrew_cache_dir=/Library/Caches/Homebrew
-  brew_cache_home=${homebrew_cache_dir}/homebrew
-  brew_url=https://github.com/homebrew/homebrew
-
-  [[ ! -d ${homebrew_cache_dir} ]] && mkdir ${homebrew_cache_dir}
-
-  if [[ ! -d ${brew_cache_home}/.git ]]; then
-    (cd ${homebrew_cache_dir} && git clone ${brew_url})
-  else
-    (cd ${brew_cache_home} && git pull)
-  fi
-  [[ $? != 0 ]] && echo git clone/pull failed && exit 127
-
-  rsync --hard-links --checksum -avz --progress --delete --delete-before ${brew_cache_home}/ ${brew_home}
-
-  PATH=${brew_home}/bin:${PATH}
-}
-
 function homebrew_setup {
-  homebrew_sync
-
   # Find out when crap breaks faster...ish
   set -e
+  echo "Making sure the xcode tools run"
+  echo sudo xcodebuild -license accept
+  sudo xcodebuild -license accept
+
+  if [[ ! -d ${brew_home} ]]; then
+    echo sudo mkdir -p ${brew_home}
+    sudo mkdir -p ${brew_home}
+  fi
+
+  if [[ ! -d ${brew_bin} ]]; then
+    echo sudo mkdir -p ${brew_bin}
+    sudo mkdir -p ${brew_bin}
+  fi
+
+  echo sudo chown ${iam_user}:${iam_group} ${brew_bin} ${brew_home}
+  sudo chown ${iam_user}:${iam_group} ${brew_bin} ${brew_home}
+
+  ruby -e \
+    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
   export HOMEBREW_BUILD_FROM_SOURCE=yesplease
+  export PATH=${brew_bin}:${PATH}
 
-  brew install reattach-to-user-namespace
-  brew install tmux --wrap-pbcopy-and-pbpaste
-  brew install mobile-shell
-
-  # So I can get git/gpg up faster, also copy the netrc credential helper
-  # so I can use gpg to decrypt ~/.netrc.gpg
-  brew install gpg git
-
-  gitver=$(brew info git 2>&1 | head -n 1 | awk '{print $3}' | sed -e 's/,//g')
-  gitinstdir=${brew_home}/Cellar/git/${gitver}
-  gitcontribnetrc=${gitinstdir}/share/git-core/contrib/credential/netrc/git-credential-netrc
-gitlibexecnetrc=${gitinstdir}/libexec/git-core/git-credential-netrc
-  ln -sf ${gitcontribnetrc} ${gitlibexecnetrc}
-
-  brew install htop
-
-  # My brew taps.
-  brew tap mitchty/yuck
-  brew tap mitchty/clang-scan-view
-  brew tap mitchty/clang-scan-build
-  brew tap mitchty/perl520
-
-  # More up to date rsync
-  brew tap homebrew/dupes
-
-  # R can be useful
-  brew tap homebrew/science
-
-  # For more up to date perl/llvm at times
-  brew tap homebrew/versions
-
-  # Install ansible for stuff.
+  echo brew install ansible
   brew install ansible
-
-  # This video player is rather spartan, and nice
-  brew tap mpv-player/mpv
-
-  # make a cocoa emacs, cause normal emacs on osx is shit
-  brew install emacs --cocoa --srgb
-
-  # Need to rebuild this prior to python otherwise things break on compile.
-  brew install llvm --with-clang --disable-assertions
-
-  # Install curl with openssl and aysnc dns resolution.
-  # Force links so we can use this curl
-  brew install curl --with-openssl --with-ares
-
-  # mpv is a nice little player compared to vlc, though now it requires
-  # docutils to compile, what the shit, keeping track of HEAD is annoying.
-  brew install readline
-  brew install sqlite
-  brew install gdbm
-  brew install python --universal --framework
-  brew install docbook
-  brew install asciidoc
-  brew install brew-pip
-  brew install libxml2 libxslt
-
-  # GOD I HATE MAVERICKS, for whatever reason this isn't
-  # included by default for only Mavericks in the platform headers.
-  # So fine, I'll install my own libxml/xslt, with blackjack and ...
-  brew link --force libxml2 libxslt
-
-  pip install docutils
-  pip install howdoi
-
-  # Install this derp ffmpeg video player.
-  brew install mpv --with-bundle
-
-  # just install vanilla postgres no language support needed really.
-  brew install postgres --no-perl --no-tcl --without-python
-
-  # Setup ruby junk
-  brew install ruby
-  gem install --no-ri --no-rdoc pry maid jist
-
-  # perl crap
-  # most of the perl modules are for irssi plugins
-  brew install perl520
-  export PATH=$(brew --prefix perl520):${PATH}
-  curl -kL http://cpanmin.us | \
-    $(brew --prefix perl520)/bin/perl - App::cpanminus
-  $(brew --prefix perl520)/bin/cpanm\
-    App::rainbarf \
-    Perl::Tidy \
-    Perl::Critic \
-    Encode \
-    Storable \
-    List::Util \
-    Hash::Util \
-    IO::Socket
-
-  # install the "rest", aka make osx a bit more useful/unixy to use.
-  brew install \
-    youtube-dl \
-    irssi \
-    yuck \
-    clang-scan-view \
-    clang-scan-build \
-    ag \
-    openssl \
-    pigz \
-    xz \
-    pixz \
-    pbzip2 \
-    pv \
-    keychain \
-    iperf \
-    nmap\
-    sntop \
-    rsync \
-    entr \
-    iftop \
-    tree \
-    pngcrush \
-    wget \
-    ispell \
-    python3 \
-    mercurial \
-    ack \
-    ncurses \
-    go \
-    gcc
 }
 
 function cabal_init
@@ -221,7 +109,7 @@ function cabal_init
 
   cabal update
 
-  echo "remote-repo: stackage:http://www.stackage.org/stackage/93a8bcca1e05c6bdc30ff391ecad800e688bd268" >> ~/.cabal/config
+  echo "remote-repo: stackage:http://www.stackage.org/stackage/93a8bcca1e05c6bdc30ff391ecad800e688bd268" >> ${dotcabal}/config
 
   cabal update
 
@@ -256,7 +144,21 @@ function cabal_backup
   (cd ${HOME} &&  tar cf - ${dotcabal} ${dotghc} | pixz -9 -o ${backup_file})
 }
 
+function ansible
+{
+  cd ${base_home}
+
+  if [[ "$(uname)" == "Darwin" ]]; then
+    echo sudo ansible-playbook -i inventory osx-root.yml
+    sudo ansible-playbook -i inventory osx-root.yml
+    echo ansible-playbook -i inventory osx-user.yml
+  fi
+}
+
 case $1 in
+ansible)
+  ansible
+  ;;
 cabal)
   cabal_init
   ;;
@@ -266,20 +168,12 @@ cabal-backup)
 homebrew)
   homebrew_setup
   ;;
-homebrew_sync)
-  homebrew_sync
-  ;;
-osx)
-  osx_setup
-  ;;
-home)
-  $0 default
-  if [[ "$(os_type)" == "osx" ]]; then
-    $? && $0 osx_setup && $0 homebrew
-  fi
-  exit $?
-  ;;
 *)
   default
+  if [[ "$(uname)" == "Darwin" ]]; then
+    echo "on osx, going to install homebrew+ansible"
+    ${iam} homebrew
+  fi
+  exit $?
   ;;
 esac
