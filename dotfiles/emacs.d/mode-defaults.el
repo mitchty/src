ca506d1d3d18d7a862121c6013760441db392861
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-11-21 13:19:16 -0600

    Update mode defaults a bit to add highlighting.

--- dotfiles/emacs.d/mode-defaults.el
+++ dotfiles/emacs.d/mode-defaults.el
@@ -1,4 +1,8 @@
 ;;-*-mode: emacs-lisp; coding: utf-8;-*-
+(interactive)
+(highlight-lines-matching-regexp ".\{81\}" 'hi-green-b)
+(hi-lock-mode)
+(hl-line-mode)
 (auto-complete-mode)
 (whitespace-mode)
 (smartparens-mode)
